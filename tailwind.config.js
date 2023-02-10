5e9f518fad76791c31954b3b7a97cdc218bfc78a
AbrahamDN <abe.ndinga@gmail.com>
2023-02-10 00:11:09 +0000

    refactor: add custom fontSize to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xm: "13px",
+      sm: "14px",
+      md: "16px",
+      base: "18px",
+      lg: ["20px", { fontWeight: "700" }],
+      xl: ["22px", { fontWeight: "700" }],
+      "2xl": ["28px", { fontWeight: "700" }],
+      "3xl": ["36px", { fontWeight: "700" }],
+      "4xl": ["58px", { fontWeight: "700" }],
+    },
     extend: {},
   },
   plugins: [],
