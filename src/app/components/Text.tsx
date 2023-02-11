1fa8b87b1f61dc93e8e1109e9463fe398e034873
AbrahamDN <abe.ndinga@gmail.com>
2023-02-11 01:03:04 +0000

    refactor: Text - add classname prop

--- src/app/components/Text.tsx
+++ src/app/components/Text.tsx
@@ -2,9 +2,15 @@ type TextProps = {
   as?: keyof JSX.IntrinsicElements;
   children: any;
   level?: 1 | 2 | 3 | 4 | "base";
+  className?: string;
 };
 
-const Text = ({ as: Element = "p", children, level = "base" }: TextProps) => {
+const Text = ({
+  as: Element = "p",
+  children,
+  level = "base",
+  className,
+}: TextProps) => {
   const classNames: { [key: number | string]: string } = {
     1: "text-3xl lg:text-4xl",
     2: "text-xl lg:text-2xl",
@@ -13,7 +19,11 @@ const Text = ({ as: Element = "p", children, level = "base" }: TextProps) => {
     base: "text-sm md:text-md lg:text-base",
   };
 
-  return <Element className={`${classNames[level]}`}>{children}</Element>;
+  return (
+    <Element className={`${classNames[level]} ${className}`}>
+      {children}
+    </Element>
+  );
 };
 
 export default Text;
