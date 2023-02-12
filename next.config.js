83550ae661bb7f5879e51aaad8064b60e7913175
AbrahamDN <abe.ndinga@gmail.com>
2023-02-12 07:09:37 +0000

    feat: add /pokemon/[id] route & redirect / & /pokemon to that route

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/pokemon/1",
+        permanent: true,
+      },
+      {
+        source: "/pokemon",
+        destination: "/pokemon/1",
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
