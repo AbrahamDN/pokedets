000b14522efc245e8820ec6df2dffe661fc7b5e5
AbrahamDN <abe.ndinga@gmail.com>
2023-02-13 02:45:49 +0000

    refactor: getPokemon to include .about & .evolutions

--- src/app/utils/getPokemon.tsx
+++ src/app/utils/getPokemon.tsx
@@ -1,9 +1,33 @@
-import { POKEMON_LIST_SCHEMA, POKEMON_SCHEMA } from "../data/pokemonList.types";
+import { EVOLUTION_CHAIN_SCHEMA } from "../data/evolutionChain.types";
+import {
+  POKEMON_LIST_SCHEMA,
+  POKEMON_SCHEMA,
+  POKEMON_SPECIES_SCHEMA,
+} from "../data/pokemonList.types";
+
+async function fetchData<Type>(url: URL | string) {
+  const data = await fetch(url);
+  return data.json() as Type;
+}
 
 export const getPokemon = async (id: string | number) => {
-  return (await (
+  const pokemon = (await (
     await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
   ).json()) as POKEMON_SCHEMA;
+
+  const species = await fetchData<POKEMON_SPECIES_SCHEMA>(pokemon.species?.url);
+  const evolutions = await fetchData<EVOLUTION_CHAIN_SCHEMA>(
+    species.evolution_chain?.url
+  );
+  const newPokemon = {
+    ...pokemon,
+    evolutions,
+    about: species.flavor_text_entries.filter(
+      (entries) => entries.language.name === "en"
+    )[0].flavor_text,
+  };
+
+  return newPokemon;
 };
 
 export const getAllPokemon = async () => {
